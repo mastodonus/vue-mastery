3428cba9d37c5cecf4d171a41c2ad6578562d85a
mastodonus <adam.js.osmond@gmail.com>
2022-07-24 22:50:44 -0300

    intro-to-vue-3 - L11

--- Intro-to-Vue-3/main.js
+++ Intro-to-Vue-3/main.js
@@ -7,12 +7,7 @@ const app = Vue.createApp({
     },
     methods: {
         updateCart(id) {
-            this.cart.push(id);
-        },
-        removeCart(id){
-            const index = this.cart.indexOf(id);
-            if(index !== -1)
-                this.cart.splice(index, 1);
+            this.cart.push(id)
         }
     }
 })
