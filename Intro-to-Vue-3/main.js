fe28a94b16eaae1f4ff5a957c1884e1a1071614e
mastodonus <adam.js.osmond@gmail.com>
2022-07-24 21:47:12 -0300

    intro-to-vue-3 - L7

--- Intro-to-Vue-3/main.js
+++ Intro-to-Vue-3/main.js
@@ -3,8 +3,9 @@ const app = Vue.createApp({
         return {
             cart:0,
             product: 'Socks',
+            brand: 'Vue Mastery',
             image: './assets/images/socks_blue.jpg',
-            inStock: true,
+            inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
@@ -13,13 +14,11 @@ const app = Vue.createApp({
         }
     },
     methods: {
-        addToCart(increment) {
-            this.cart = (this.cart + increment) > 0
-                ? this.cart + increment
-                : 0;
+        addToCart() {
+            this.cart += 1
         },
-        updateImage(variantImage){
-            this.image = variantImage;
+        updateImage(variantImage) {
+            this.image = variantImage
         }
     }
 })
